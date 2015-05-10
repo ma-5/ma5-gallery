4c29f4a6af73b67516285e0ca4b2ca88c5446f96
Tomasz Kalinowski <tomaszka72@gmail.com>
2015-05-10 12:38:51 +0200

    ma5-gallery v1.2

--- javascripts/ma5gallery.js
+++ javascripts/ma5gallery.js
@@ -1,21 +1,25 @@
 /*
 *   MA5Gallery
-*   v 1.1
+*   v 1.2
 *   Copyright (c) 2015 Tomasz Kalinowski
 *   http://galeria.ma5.pl
 *   GitHub: https://github.com/ma-5/ma5-gallery
 */ 
 ;
+$.fn.ma5preload = function() {
+    $('body').append('<div class="ma5-preloadbox"></div>');
+    this.each(function(){$(this).clone().attr('src', $(this).attr('src').replace(/\-thumbnail./, '.')).appendTo('.ma5-preloadbox');});  
+}
 function ma5showActive() {
     $('.ma5-imgbox').addClass('ma5-previous');
     $('body').addClass('ma5-in').append('<div class="ma5-imgbox"></div>');
     setTimeout(function() {$('.ma5-imgbox.ma5-previous').remove();$('body').removeClass('ma5-in');}, 1000);
     var last = $('.ma5-imgbox').last();
     $('.ma5-active img').clone().attr('src', $('.ma5-active img').attr('src').replace(/\-thumbnail./, '.')).addClass('ma5-clone').appendTo(last);
-    
 }
 function ma5hideActive() {
     $('.ma5-imgbox').on('touch click', function() {
+        $('figure').removeClass('ma5-active');    
         $('.ma5-imgbox').addClass('ma5-out');
         var content = $(".ma5-bg").contents();
         $('.ma5-gallery').addClass('ma5-out');
@@ -31,8 +35,11 @@ function ma5hideActive() {
         }, 1600);
     });
 }
-function ma5gallery(img) {
-    $(img).on('touch click', function() {
+$.fn.ma5gallery = function(atributes) {
+    if(atributes.preload == true) {
+        $(this).ma5preload();
+    }
+    $(this).on('touch click', function() {
         if(!$('.ma5-imgbox').hasClass('ma5-out') && !$('body').hasClass('ma5-in') ) {
             if($(this).parent().parent().hasClass('ma5-gallery') || $(this).parent().parent().hasClass('ma5-bg')) {
                 // gallery mode
@@ -50,18 +57,9 @@ function ma5gallery(img) {
                 }
             } else {
                 //single mode
-                var fThis = $(this);
-                $('body').append('<div class="ma5-imgbox ma5-animated"></div>');
-                setTimeout(function() {$('.ma5-imgbox').removeClass('ma5-animated')}, 800);
-                var last = $('.ma5-imgbox').last();
-                $(this).clone().attr('src', $(this).attr('src').replace(/\-thumbnail./, '.')).addClass('ma5-clone').appendTo(last);
-                $('.ma5-imgbox').on('touch click', function() {
-                    if (!$('.ma5-imgbox').hasClass('ma5-animated')) {
-                        $(this).addClass('ma5-out');
-                        var thisobject = $(this);
-                        setTimeout(function() {thisobject.remove(); fThis.parent()}, 800);
-                    }
-                });
+                $(this).parent().addClass('ma5-active');
+                ma5showActive();
+                ma5hideActive();
             }
         }
     });
