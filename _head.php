049a0079c4bdf70c9631a8e5b2cecf3255ed1421
ma-5 <ma-5@users.noreply.github.com>
2015-11-20 22:39:35 +0100

    ma5-gallery v.2.0.0 fullscreen and movie player features

--- _head.php
+++ _head.php
@@ -6,15 +6,15 @@
         <meta name="keywords" content="ma5gallery, galeria html5, galeria jquery">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui, user-scalable=no">
-        <link href="./stylesheets/bootstrap.min.css" rel="stylesheet" type="text/css">
-        <link href="./stylesheets/screen.css" rel="stylesheet" type="text/css">
-        <link href="./stylesheets/ma5gallery.css" rel="stylesheet" type="text/css">
-        <script src="./javascripts/jquery.js"></script>
+        <link href="./css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="./css/screen.css" rel="stylesheet" type="text/css">
+        <link href="./css/ma5gallery.css" rel="stylesheet" type="text/css">
+        <script src="./js/jquery.js"></script>
         
-        <script src="./javascripts/ma5gallery.js"></script>
-        <script src="./javascripts/functions.js"></script>
+        <script src="./js/ma5gallery.js"></script>
+        <script src="./js/functions.js"></script>
         <!--[if lt IE 9]>
-        <script src="./javascripts/html5.js"></script>
+        <script src="./js/html5.js"></script>
         <![endif]-->
     </head>
     <body>
